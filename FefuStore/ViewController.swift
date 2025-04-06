1177129f08e55b75e91e1378a4de039253ca69b2
Andrew <maneki.kakutasu@gmail.com>
2025-04-06 14:47:13 +1000

    Initial Commit

---
+++ FefuStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FefuStore
+//
+//  Created by Андрей Германович on 06.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
